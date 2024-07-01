0613e6c565f6e8167ecb244f8ab4901777b7f67b
Samuel Gunter <samathingamajig@gmail.com>
2024-07-01 00:26:03 -0700

    feat: basic sowing, changed how GetState orders pits

--- game/game.go
+++ game/game.go
@@ -1,10 +1,14 @@
 package game
 
+import (
+	"fmt"
+)
+
 const SIZE = 6
 const INITIAL_SEEDS = 4
 
-type Player = uint8
-type GameStatus = uint8
+type Player uint8
+type Status uint8
 
 const (
 	PLAYER_ONE Player = iota
@@ -12,7 +16,7 @@ const (
 )
 
 const (
-	FRESH GameStatus = iota
+	FRESH Status = iota
 	STARTED
 	FINISHED
 )
@@ -21,29 +25,32 @@ type MancalaGameState struct {
 	Pits       [2][SIZE]uint
 	Stores     [2]uint
 	NextPlayer Player
-	Status     GameStatus
+	Status     Status
 }
 
 type MancalaGame struct {
-	Pits       [SIZE*2 + 2]uint
+	pits       [SIZE*2 + 2]uint
 	nextPlayer Player
-	status     GameStatus
+	status     Status
 }
 
 func New() *MancalaGame {
 	game := &MancalaGame{
-		Pits:       [SIZE*2 + 2]uint{},
+		pits:       [SIZE*2 + 2]uint{},
 		nextPlayer: PLAYER_ONE,
 		status:     FRESH,
 	}
 	for i := 0; i < SIZE; i++ {
-		game.Pits[i] = INITIAL_SEEDS
-		game.Pits[i+SIZE+1] = INITIAL_SEEDS
+		game.pits[i] = INITIAL_SEEDS
+		game.pits[i+SIZE+1] = INITIAL_SEEDS
 	}
 
 	return game
 }
 
+// Returns the current state of the Mancala game
+// Clients are responsible for any rotating of boards
+// (i.e. pits are returned from the POV of each respective player viewing)
 func (g *MancalaGame) GetState() MancalaGameState {
 	state := MancalaGameState{
 		Pits:       [2][SIZE]uint{},
@@ -53,12 +60,65 @@ func (g *MancalaGame) GetState() MancalaGameState {
 	}
 
 	for i := 0; i < SIZE; i++ {
-		state.Pits[0][i] = g.Pits[i]
-		state.Pits[1][SIZE-1-i] = g.Pits[SIZE+1+i]
+		state.Pits[0][i] = g.pits[i]
+		state.Pits[1][i] = g.pits[SIZE+1+i]
 	}
 
-	state.Stores[0] = g.Pits[SIZE]
-	state.Stores[1] = g.Pits[SIZE*2+1]
+	state.Stores[0] = g.pits[SIZE]
+	state.Stores[1] = g.pits[SIZE*2+1]
 
 	return state
+}
+
+func (g *MancalaGame) Sow(player Player, position uint) (Player, Status, error) {
+	if g.status == FINISHED {
+		return g.nextPlayer, g.status, fmt.Errorf("attempted to sow after game is over")
+	}
+
+	if g.nextPlayer != player {
+		return g.nextPlayer, g.status, fmt.Errorf("attempted to sow with the wrong next player")
+	}
+
+	if position >= SIZE {
+		return g.nextPlayer, g.status, fmt.Errorf("attempted to sow out of range: %d", position)
+	}
+
+	g.status = STARTED
+
+	idx := position
+	if player == PLAYER_TWO {
+		idx += SIZE + 1
+	}
+
+	seeds := g.pits[idx]
+	g.pits[idx] = 0
+
+	for ; seeds > 0; seeds-- {
+		idx++
+		if (idx == SIZE && player == PLAYER_TWO) || (idx == 2*SIZE+1 && player == PLAYER_ONE) {
+			// In a store not belonging to a player, proceed to next pit
+			idx++
+		}
+		if idx == SIZE*2+2 {
+			// Wrap around
+			idx = 0
+		}
+
+		g.pits[idx]++
+	}
+
+	// Swap players, unless ending in a store
+	if idx%(SIZE+1) != SIZE {
+		if player == PLAYER_ONE {
+			g.nextPlayer = PLAYER_TWO
+		} else {
+			g.nextPlayer = PLAYER_ONE
+		}
+	}
+
+	// TODO: Ending in (previously) empty pit on your side lets you steal
+
+	// TODO: Ending states (empty row for either player, player with seeds on their side can collect for their store)
+
+	return g.nextPlayer, g.status, nil
 }
--- game/game_test.go
+++ game/game_test.go
@@ -50,3 +50,52 @@ func TestNewGameState(t *testing.T) {
 		t.Error("A new game should be the 'FRESH' status\n")
 	}
 }
+
+func TestFirstMoveSuccess(t *testing.T) {
+	g := game.New()
+
+	nextPlayer, status, err := g.Sow(game.PLAYER_ONE, 2)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	if nextPlayer != game.PLAYER_ONE {
+		t.Error("The next player should be player one, since they ended with a scoring move")
+	}
+
+	if status != game.STARTED {
+		t.Error("The game status should be 'STARTED'")
+	}
+
+	state := g.GetState()
+
+	expectedPits := [2][game.SIZE]uint{{4, 4, 0, 5, 5, 5}, {4, 4, 4, 4, 4, 4}}
+	actualPits := state.Pits
+	pitsEqual := true
+	for i := range expectedPits {
+		for j := range expectedPits[i] {
+			// t.Logf("%d, %d", i, j)
+			if expectedPits[i][j] != actualPits[i][j] {
+				pitsEqual = false
+			}
+		}
+	}
+
+	if !pitsEqual {
+		t.Errorf("Pits are not equal! Expected %v, got %v\n", expectedPits, actualPits)
+	}
+
+	expectedStores := [2]uint{1, 0}
+	actualStores := state.Stores
+	storesEqual := true
+	for i := range expectedStores {
+		if expectedStores[i] != actualStores[i] {
+			storesEqual = false
+		}
+	}
+
+	if !storesEqual {
+		t.Errorf("Stores are not equal! Expected %v, got %v\n", expectedStores, actualStores)
+	}
+}
